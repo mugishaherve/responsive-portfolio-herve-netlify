76edde460007042c1c2263b622f34e6b9c09f95d
herve <mugishaherve1000@gmail.com>
2023-04-25 13:05:02 +0200

    my portfolio

---
+++ script.js
@@ -0,0 +1,59 @@
+
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height
+        ) {
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    })
+
+    let header = document.querySelector('header')
+
+    header.classList.toggle('sticky', window.scrollY > 100)
+
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+}
+
+const toggleSwitch = document.querySelector('.toggle-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
